8d0013644b09f1655b765d67886ea7f7b06e92ce
Illensky <alexis.laroche.02240@gmail.com>
2021-12-09 13:59:20 +0100

    exercice done

---
+++ app.js
@@ -0,0 +1,19 @@
+const start = document.querySelector("#interval-start")
+const stop = document.querySelector("#interval-stop")
+const div = document.querySelector("#interval-div")
+let timer = 0;
+
+let timeOutRecursive = function () {
+    let timeOutId = setTimeout(function () {
+        if (timer <= 600){
+            timer += 60
+            div.innerHTML = timer.toString()
+            timeOutRecursive()
+        }
+    }, 600)
+    stop.addEventListener("click", () => {
+        clearTimeout(timeOutId);
+    })
+}
+
+start.addEventListener("click", timeOutRecursive)
